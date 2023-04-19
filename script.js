295ad04beea0ea652d149f1f8a3c2538c7511e61
Nurdaulet Shinpolatov <nshinpolatov@gmail.com>
2023-04-19 18:58:36 +0500

    asd

---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.querySelector(".form");
+const list = document.querySelector(".todosList");
+const clear = document.querySelector(".clear");
+
+const todos = [{ value: "Reading a book" }, { value: "Playing football" }];
+
+const render = () => {
+    list.innerHTML = "";
+    for (const element of todos) {
+        list.innerHTML += `
+        <li class="todo">
+            <input value="${element.value}" class="todo_input" type="text" />
+            <div class="edit">
+                <i class="bx bx-sm bxs-pencil"></i>
+            </div>
+            <div class="delete">
+                <i class="bx bx-sm bx-trash"></i>
+            </div>
+        </li>
+        `
+    }
+}
+render();
+
+const clearTodos = () => {
+    while (todos.length != 0) {
+        todos.pop();
+    }
+    list.innerHTML = "";
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+    const inputValue = event.target["todo"].value;
+    const newTodo = { value: inputValue, id: Date.now() + "#" };
+    todos.unshift(newTodo);
+    render();
+    inputValue = "";
+});
+
+clear.addEventListener("click", (event) => {
+    clearTodos();
+})
