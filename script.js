c5131da35255abab171bb3796c7b210ac6f03790
Nurdaulet Shinpolatov <99029072+NurdauletShinpolatov@users.noreply.github.com>
2023-05-08 09:24:28 +0500

    undoing the change

--- script.js
+++ script.js
@@ -1,306 +1,205 @@
-const list = document.querySelector(".tasks_list");
-const inputAdd = document.querySelector(".new_task");
-const filterTasks = document.querySelector(".filter_tasks_by_completion");
-const block = document.querySelector(".block");
-let toggleDateCheckbox = document.querySelector(".toggleDate");
 
-let now = new Date();
-now =
-  now.getMonth() +
-  "/" +
-  now.getDay() +
-  "/" +
-  now.getFullYear() +
-  " " +
-  now.getHours() +
-  ":" +
-  now.getMinutes();
+/* 
+    What should we do:
+    1. Sweet alert
+    2. 
+*/
+
+
+const list = document.querySelector(".todosList");
+const inputAdd = document.querySelector(".input");
+const filterTodos = document.querySelector(".filterTodos");
+const block = document.querySelector(".block");
 
 // states
-// JSON.parse(localStorage.getItem("tasks"))
-let tasks = false || [
-  {
-    value: "Malumotlar LOCAL STORAGE da saqlanadi",
-    id: "a1",
-    isDone: false,
-    date: now,
-  },
-  {
-    value: "DRAG qilib vaziyfalarning tartiblang",
-    id: "a2",
-    isDone: false,
-    date: now,
-  },
-  {
-    value: "Ozgartirib bolgach ENTER bosing",
-    id: "a3",
-    isDone: false,
-    date: now,
-  },
-  { value: "CRUD", id: "a4", isDone: false, date: now },
-  {
-    value: "BAJARILGAN vaziyfani belgilasa boladi",
-    id: "a5",
-    isDone: false,
-    date: now,
-  },
-  {
-    value: "barajilgan/bajarilmaganlarni FILTRLANG",
-    id: "a6",
-    isDone: false,
-    date: now,
-  },
+let todos = JSON.parse(localStorage.getItem("todos")) || [
+    { value: "Malumotlar LOCAL STORAGE da saqlanadi", id: "a1", isDone: false },
+    { value: "DRAG qilib vaziyfalarning tartiblang", id: "a2", isDone: false },
+    { value: "Ozgartirib bolgach ENTER bosing", id: "a3", isDone: false },
+    { value: "CRUD", id: "a4", isDone: false },
+    { value: "BAJARILGAN vaziyfani belgilasa boladi", id: "a5", isDone: false },
+    { value: "barajilgan/bajarilmaganlarni FILTRLANG", id: "a6", isDone: false },
 ];
 let selectedFilter = "all";
 let selectedTask = "";
-let showDate = false;
 
-const filterByStatus = (tasks, selectedFilter) => {
-  switch (selectedFilter) {
-    case "completed":
-      return tasks.filter((item) => item.isDone);
-    case "inProgress":
-      return tasks.filter((item) => !item.isDone);
-    default:
-      return tasks;
-  }
-};
+
+
+const filterByStatus = (todos, selectedFilter) => {
+    switch (selectedFilter) {
+        case "completed":
+            return todos.filter((item) => item.isDone);
+        case "inProgress":
+            return todos.filter((item) => !item.isDone);
+        default:
+            return todos;
+    }
+}
 
 const dragReplaceTodo = () => {
-  const start = document.querySelector(".dragStarted");
-  const end = document.querySelector(".dragDropped");
-  const startId = start.id;
-  const endId = end.id;
-  if (startId != endId) {
-    let startObject;
-    for (let index = 0; index < tasks.length; index++) {
-      if (tasks[index].id == startId) {
-        startObject = { ...tasks[index] };
-        tasks.splice(index, 1);
-        break;
-      }
+    const start = document.querySelector(".dragStarted");
+    const end = document.querySelector(".dragDropped");
+    const startId = start.id;
+    const endId = end.id;
+    if (startId != endId) {
+        let startObject;
+        for (let index = 0; index < todos.length; index++) {
+            if (todos[index].id == startId) {
+                startObject = {...todos[index]};
+                todos.splice(index, 1);
+                break;
+            }
+        }
+        for (let index = 0; index < todos.length; index++) {
+            if (todos[index].id == endId) {
+                endObject = {...todos[index]};
+                todos.splice(index, 0, startObject);
+                index += 1;
+                break;
+            }
+        }
     }
-    for (let index = 0; index < tasks.length; index++) {
-      if (tasks[index].id == endId) {
-        endObject = { ...tasks[index] };
-        tasks.splice(index, 0, startObject);
-        index += 1;
-        break;
-      }
-    }
-  }
-
-  render(123);
-};
+    
+    render(123);
+}
 
 const dragAndDrop = () => {
-  const tasks = document.querySelectorAll(".task");
+    const tasks = document.querySelectorAll(".todo");
+    
+    const dragStart = function () {
+        this.classList.add("dragStarted");
+    }
+    const dragEnd = function () {
+        this.classList.remove("dragHovered");
+        this.classList.remove("dragStarted");
+    }
+    const dragOver = function (event) {
+        this.classList.add("dragHovered");
+        event.preventDefault();
+    }
+    const dragEnter = function (event) {
+        event.preventDefault();
+    }
+    const dragLeave = function () {
+        this.classList.remove("dragHovered");
+    }
+    const dragDrop = function () {
+        this.classList.add("dragDropped");
+        this.classList.remove("dragHovered");
+        dragReplaceTodo();
+    }
 
-  const dragStart = function () {
-    this.classList.add("dragStarted");
-  };
-  const dragEnd = function () {
-    this.classList.remove("dragHovered");
-    this.classList.remove("dragStarted");
-  };
-  const dragOver = function (event) {
-    this.classList.add("dragHovered");
-    event.preventDefault();
-  };
-  const dragEnter = function (event) {
-    event.preventDefault();
-  };
-  const dragLeave = function () {
-    this.classList.remove("dragHovered");
-  };
-  const dragDrop = function () {
-    this.classList.add("dragDropped");
-    this.classList.remove("dragHovered");
-    dragReplaceTodo();
-  };
-
-  tasks.forEach((elem) => {
-    elem.addEventListener("dragstart", dragStart);
-    elem.addEventListener("dragend", dragEnd);
-    elem.addEventListener("dragover", dragOver);
-    elem.addEventListener("dragenter", dragEnter);
-    elem.addEventListener("dragleave", dragLeave);
-    elem.addEventListener("drop", dragDrop);
-  });
-};
+    tasks.forEach((elem) => {
+        elem.addEventListener("dragstart", dragStart);
+        elem.addEventListener("dragend", dragEnd);
+        elem.addEventListener("dragover", dragOver);
+        elem.addEventListener("dragenter", dragEnter);
+        elem.addEventListener("dragleave", dragLeave);
+        elem.addEventListener("drop", dragDrop);
+    });
+}
 
 const render = () => {
-  // localStorage.setItem('tasks', JSON.stringify(tasks));
-  list.innerHTML = "";
-  // console.log("render");
-  filterByStatus(tasks, selectedFilter).forEach((task) => {
-    list.innerHTML += `
-        <li draggable="true" id="${task.id}" class="task ${
-      task.isDone ? "completedTask" : ""
-    }">
-            <input type="checkbox" ${
-              task.isDone ? "checked" : ""
-            } class="toggleCheck" />
-            <div class="task_value">
-                <input value="${task.value}" class="task_input ${
-      selectedTask != task.id ? "notEditable" : ""
-    } ${task.isDone ? "completedTask" : ""}" type="text" />
-                <p class="task_date ${showDate == true ? "" : "none"}">${
-      task.date
-    }</p>
-            </div>
-            <div class="save ${selectedTask != task.id ? "none" : ""}">
+    localStorage.setItem('todos', JSON.stringify(todos));
+    list.innerHTML = "";
+    filterByStatus(todos, selectedFilter).forEach(element => {
+        list.innerHTML += `
+        <li draggable="true" id="${element.id}" class="todo ${element.isDone ? "completedTask" : ""}">
+            <input type="checkbox" ${element.isDone ? "checked" : ""} />
+            <input value="${element.value}" class="todo_input ${selectedTask != element.id ? 'notEditable' : ''} ${element.isDone ? 'completedTask' : ''}" type="text" />
+            <div class="save ${selectedTask != element.id ? 'none' : ''}">
                 <i class='bx bx-sm bxs-save'></i>
             </div>
-            <div class="cancel ${selectedTask != task.id ? "none" : ""} ">
+            <div class="cancel ${selectedTask != element.id ? 'none' : ''} ">
                 <i class='bx bx-sm bx-x'></i>
             </div>
-            <div class="edit ${selectedTask == task.id ? "none" : ""}">
+            <div class="edit ${selectedTask == element.id ? 'none' : ''}">
                 <i class="bx bx-sm bxs-pencil"></i>
             </div>
-            <div class="delete ${selectedTask == task.id ? "none" : ""}">
+            <div class="delete ${selectedTask == element.id ? 'none' : ''}">
                 <i class="bx bx-sm bx-trash"></i>
             </div>
         </li>
-        `;
-  });
-  dragAndDrop();
-};
+        `
+    });
+    dragAndDrop();
+}
 render();
 
-const toggleCheck = (id) => {
-  tasks = tasks.map((item) =>
-    item.id == id ? { ...item, isDone: !item.isDone } : item
-  );
-  render();
-};
+const markAsDone = (id) => {
+    todos = todos.map((item) => (item.id == id ? {...item, isDone: !item.isDone} : item));
+    render();
+}
 
 const addNewTask = () => {
-  if (inputAdd.value == "") {
-    Swal.fire({
-      position: "top-end",
-      icon: "info",
-      title: "Task should not be empty",
-      showConfirmButton: false,
-      timer: 1000,
-    });
-  } else {
-    let now = new Date();
-    now =
-      now.getMonth() +
-      "/" +
-      now.getDay() +
-      "/" +
-      now.getFullYear() +
-      " " +
-      now.getHours() +
-      ":" +
-      now.getMinutes();
-    const newTodo = {
-      value: inputAdd.value,
-      id: "a" + Date.now(),
-      date: now,
-      isDone: false,
-    };
-    tasks.unshift(newTodo);
-    inputAdd.value = "";
-    render();
-  }
-};
+    if (inputAdd.value == "") {
+        alert("Input field should not be empty!");
+    } else {
+        const newTodo = { value: inputAdd.value, id: "a" + Date.now(), isDone: false, edit: false };
+        todos.unshift(newTodo);
+        inputAdd.value = "";
+        render();
+    }
+}
 
 const clearAllTasks = () => {
-  tasks = [];
-  selectedTask = "";
-  render();
-};
+    todos = [];
+    selectedTask = "";
+    render();
+}
 
 const deleteTask = (id) => {
-  Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: "Deleted!",
-        text: "Task has been deleted.",
-        icon: "success",
-        showConfirmButton: false,
-        timer: 1000,
-      });
-      console.log(id);
-      tasks = tasks.filter((item) => item.id !== id);
-      selectedTask = "";
-      render();
-    }
-  });
-};
+    todos = todos.filter((item) => item.id !== id);
+    selectedTask = "";
+    render();
+}
 
 const enableEditTask = (id) => {
-  selectedTask = id;
-  render();
-  const todoInput = document.querySelector(`#${selectedTask} .task_input`);
-  todoInput.addEventListener("keydown", (e) => {
-    if (e.keyCode === 13) {
-      saveEditedTask();
-    }
-  });
-  todoInput.setSelectionRange(todoInput.value.length, todoInput.value.length);
-  todoInput.focus();
-};
+    selectedTask = id;
+    render();
+    const todoInput = document.querySelector(`#${selectedTask} .todo_input`);
+    todoInput.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+            saveEditedTask();
+        }
+    });
+    todoInput.setSelectionRange(todoInput.value.length, todoInput.value.length);
+    todoInput.focus();
+}
 
 const cancelEdit = () => {
-  selectedTask = "";
-  render();
-};
+    selectedTask = "";
+    render();
+}
 
 const saveEditedTask = () => {
-  const editedTask = document.querySelector(`#${selectedTask} .task_input`);
-  if (editedTask.value == "") {
-    alert("It shouldn't be empty");
-  } else {
-    tasks = tasks.map((item) =>
-      item.id == selectedTask ? { ...item, value: editedTask.value } : item
-    );
-  }
-  selectedTask = "";
-  render();
-};
+    const editedTask = document.querySelector(`#${selectedTask} .todo_input`);
+    if (editedTask.value == "") {
+        alert("It shouldn't be empty");
+    } else {
+        todos = todos.map((item) => (item.id == selectedTask ? {...item, value: editedTask.value} : item))
+    }
+    selectedTask = "";
+    render();
+}
 
 const changeFilter = (e) => {
-  selectedFilter = e.target.value;
-  render();
-};
-
-const toggleDate = () => {
-  if (showDate == true) {
-    showDate = false;
-    toggleDateCheckbox.outerHTML = `<input class="toggleDate" name="toggleDate" type="checkbox">`;
-  } else {
-    showDate = true;
-    toggleDateCheckbox.outerHTML = `<input class="toggleDate" checked name="toggleDate" type="checkbox">`;
-  }
-  render();
-  toggleDateCheckbox = document.querySelector(".toggleDate");
-};
+    selectedFilter = e.target.value;
+    render();
+}
 
 const handleClick = (e) => {
-  e.preventDefault();
-  const btn = e.target;
-  const id = btn.closest(".task")?.id;
-  if (btn.closest(".add_new_task")) addNewTask();
-  else if (btn.closest(".clear")) clearAllTasks();
-  else if (btn.closest(".cancel")) cancelEdit();
-  else if (btn.closest(".save")) saveEditedTask();
-  else if (btn.closest(".delete")) deleteTask(id);
-  else if (btn.closest(".edit")) enableEditTask(id);
-  else if (btn.closest(".toggleCheck")) toggleCheck(id);
-  else if (btn.closest(".toggleDate")) toggleDate();
-};
+    e.preventDefault();
+    const btn = e.target;
+    const id = btn.closest(".todo")?.id;
+    if (btn.closest(".add")) { addNewTask(); }
+    else if (btn.closest(".clear")) { clearAllTasks(); }
+    else if (btn.closest(".delete")) { deleteTask(id); }
+    else if (btn.closest(".edit")) { enableEditTask(id); }
+    else if (btn.closest(".cancel")) { cancelEdit(); }
+    else if (btn.closest(".save")) { saveEditedTask(); }
+    else if (btn.closest(".delete")) { deleteTask(); }
+}
 
-filterTasks.addEventListener("change", changeFilter);
-block.addEventListener("click", handleClick);
+filterTodos.addEventListener("change", changeFilter)
+block.addEventListener("click", handleClick)
